9e267d0e487f443cb4ebc8063464fdc131cecfaa
prashant1177 <prashantjpatil49@gmail.com>
2025-08-09 14:27:23 +0530

    Fixing error from dbConnect

--- module-2/src/config/dbConnect.js
+++ module-2/src/config/dbConnect.js
@@ -3,7 +3,7 @@ const env = require('../env/index');
 
 const dbConnect = async () => {
 	mongoose
-		.connect(env.MONGO_URI)
+		.connect(process.env.MONGO_URI)
 		.then(() => console.log('MongoDB connected'))
 		.catch((err) => console.log('database not connected', err));
 };
